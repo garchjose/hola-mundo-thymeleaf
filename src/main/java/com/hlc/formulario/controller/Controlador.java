48f31313aefd976e299e35af1679b3b844b0506f
Jose Garcia <185412207+garchjose@users.noreply.github.com>
2024-11-25 15:36:04 +0100

    Solución ejemplo del hola mundo con thymeleaf

---
+++ src/main/java/com/hlc/formulario/controller/Controlador.java
@@ -0,0 +1,18 @@
+package com.hlc.formulario.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Controlador {
+    @GetMapping("/saluda")
+    public String greeting(@RequestParam(required = false, defaultValue = "World") String name, Model model) {
+        // Añadir el atributo "name" al modelo
+        model.addAttribute("name", name);
+        // Retorno del nombre de la vista (un archivo HTML Thymeleaf llamado "saludo")
+        return "saludo";
+    }
+}
+
